cb151b2bc88d75e466ba7d90147a4d0b22c2d582
kevinnarbas <kevinnarbas@gmail.com>
2021-03-30 16:03:32 -0700

    adds test directories and files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react'
-import cats from './mockCats.js'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import CatIndex from './pages/CatIndex'
-import CatShow from './pages/CatShow'
-import CatNew from './pages/CatNew'
-import CatEdit from './pages/CatEdit'
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
-import './App.css'
+import React, { Component } from 'react';
+import cats from './mockCats.js';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CatIndex from './pages/CatIndex';
+import CatShow from './pages/CatShow';
+import CatNew from './pages/CatNew';
+import CatEdit from './pages/CatEdit';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import './App.css';
 
-import {
-BrowserRouter as Router,
-Route,
-Switch
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <Router>
         <Header />
-          <Switch>
-            <Route exact path="/" component={ Home } />
-            <Route path="/catIndex" component={ CatIndex } />
-            <Route path="/catShow/:id" component={ CatShow } />
-            <Route path="/catNew" component={ CatNew } />
-            <Route path="/catEdit/:id" component={ CatEdit } />
-            <Route component={ NotFound } />
-          </Switch>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/catIndex' component={CatIndex} />
+          <Route path='/catShow/:id' component={CatShow} />
+          <Route path='/catNew' component={CatNew} />
+          <Route path='/catEdit/:id' component={CatEdit} />
+          <Route component={NotFound} />
+        </Switch>
         <Footer />
       </Router>
     );
